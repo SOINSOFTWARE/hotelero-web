d622ec30e634e0a4ed43c163e49dc201efc463d2
crodriguez <crodriguez@carlosrodriguez-inew>
2018-01-23 10:19:32 -0500

    Removing logged user validation from web project

--- src/main/java/com/soinsoftware/hotelero/web/servlet/LoginServlet.java
+++ src/main/java/com/soinsoftware/hotelero/web/servlet/LoginServlet.java
@@ -10,9 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.soinsoftware.hotelero.core.controller.UserController;
-import com.soinsoftware.hotelero.persistence.entity.Role;
+import com.soinsoftware.hotelero.core.exception.LoginException;
 import com.soinsoftware.hotelero.persistence.entity.User;
-import com.soinsoftware.hotelero.web.exception.LoginException;
 
 import lombok.extern.log4j.Log4j;
 
@@ -24,10 +23,6 @@ import lombok.extern.log4j.Log4j;
 public class LoginServlet extends AbstractServlet {
 
 	private static final long serialVersionUID = 7094727863041162821L;
-
-	private static final String MSG_NOT_LOGIN_1 = "Usuario y/o contrase&ntilde;a no validos.";
-
-	private static final String MSG_NOT_LOGIN_2 = "El usuario no tiene permiso para ingresar a Hotelero.";
 
 	protected static final String VIEW = "/login.jsp";
 
@@ -65,7 +60,7 @@ public class LoginServlet extends AbstractServlet {
 		final String password = getParameter(request, ATTRIBUTE_PASSWORD);
 		final User user = select(username, password);
 		try {
-			validateLoggedUser(user);
+			controller.validateLoggedUser(user);
 			log.info("Login data accepted...");
 			addUserToSession(request, user);
 			setDistpatcherToHome(request, response);
@@ -82,17 +77,6 @@ public class LoginServlet extends AbstractServlet {
 			log.error(ex);
 		}
 		return controller.select(username, password.toCharArray());
-	}
-
-	private void validateLoggedUser(final User user) throws LoginException {
-		if (user != null && user.isEnabled()) {
-			final Role role = user.getRole();
-			if (role == null || !role.isEnabled()) {
-				throw new LoginException(MSG_NOT_LOGIN_2);
-			}
-		} else {
-			throw new LoginException(MSG_NOT_LOGIN_1);
-		}
 	}
 
 	private RequestDispatcher setDistpatcherToHome(final HttpServletRequest request, final HttpServletResponse response)
